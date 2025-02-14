89ee1f765e731fe4e976aad3a883018abf840890
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-02-14 18:28:05 +0200

    add event listeners for passowrds and select inputs, write validation for these inputs

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,19 +5,30 @@ const emailInput = document.getElementById("email");
 const ageInput = document.getElementById("age");
 const radioGroups = document.querySelectorAll(".reg-form__radio-group");
 const occupationSelect = document.getElementById("occupation");
+const passInput = document.getElementById("pass1");
+const passTips = document.getElementById("passTips");
+const passRepeatInput = document.getElementById("pass2");
 const consentCheckbox = document.getElementById("consent");
 
 let sexChecked = false;
 let consentChecked = false;
 
+const lengthTip = document.getElementById("length");
+const upperCaseTip = document.getElementById("upperCaseLetter");
+const lowerCaseTip = document.getElementById("lowerCaseLetter");
+const digitTip = document.getElementById("digit");
+
 const nameError = document.getElementById("nameError");
 const emailError = document.getElementById("emailError");
 const ageError = document.getElementById("ageError");
+const sexError = document.getElementById("sexError");
+const occupationError = document.getElementById("occupationError");
+const passError = document.getElementById("pass1Error");
+const passRepeatError = document.getElementById("pass2Error");
 const consentError = document.getElementById("consentError");
 
-const firstCharErrorMsg = "Имя должно начинаться с буквы!";
+const firstCharErrorMsg = "Имя должно начинаться с буквы";
 const noNameErrorMsg = "Пожалуйста, введите имя";
-// const nameTooLongErrorMsg = "Длина имени не должна превышать 20 символов";
 const nameTooShortErrorMsg = "Длина имени не должна быть меньше 2 символов";
 const noEmailErrorMsg = "Пожалуйста, введите адрес электронной почты";
 const invalidEmailErrorMsg =
@@ -26,6 +37,11 @@ const noAgeErrorMsg = "Пожалуйста, введите возраст.";
 const invalidAgeErrorMsg = "Возраст не может быть меньше 1 или больше 110.";
 const noConsentErrorMsg =
   "Пожалуйста, подтвердите согласие на обработку персональных данных.";
+const noOccupationErrorMsg = "Пожалуйста, выберете профессию из списка";
+const noPassError = "Пожалуйста, введите пароль";
+const invalidPassError = "Пароль не соответствует требованиям";
+const noRepeatPassError = "Пожалуйста, повторите пароль";
+const invalidRepeatPassError = "Пароли не совпадают";
 
 function checkFirstChar(str) {
   const regex = /^[A-Za-zА-Яа-я]/;
@@ -44,56 +60,64 @@ function hideOrShowElem(elem, hide = true) {
   }
 }
 
-function changeNameOnInput() {
-  const name = nameInput.value;
-  if (checkFirstChar(name)) {
-    nameInput.value = name.slice(1);
-    pasteMsg(nameError, firstCharErrorMsg);
-    hideOrShowElem(nameError, false);
+function toggleErrorMsg(condition, element, msg) {
+  pasteMsg(element, msg);
+  if (condition) {
+    hideOrShowElem(element);
   } else {
-    pasteMsg(nameError, "");
-    hideOrShowElem(nameError);
+    hideOrShowElem(element, false);
   }
 }
 
-function validateName() {
+function changeNameOnInput() {
   const name = nameInput.value;
   let valid = true;
-  if (!name) {
-    pasteMsg(nameError, noNameErrorMsg);
-    hideOrShowElem(nameError, false);
+  let erMsg = "";
+
+  if (checkFirstChar(name)) {
+    nameInput.value = name.slice(1);
     valid = false;
-  } else if (name.length < 2) {
-    pasteMsg(nameError, nameTooShortErrorMsg);
-    hideOrShowElem(nameError, false);
-    valid = false;
-    //   } else if (name.length > 20) {
-    //     pasteMsg(nameError, nameTooLongErrorMsg);
-    //     hideOrShowElem(nameError, false);
-    //     valid = false;
-  } else {
-    hideOrShowElem(nameError);
+    erMsg = firstCharErrorMsg;
   }
 
+  toggleErrorMsg(valid, nameError, erMsg);
+}
+
+function isNameValid() {
+  const name = nameInput.value;
+  let valid;
+  let erMsg = "";
+
+  if (!name) {
+    erMsg = noNameErrorMsg;
+    valid = false;
+  } else if (name.length < 2) {
+    valid = false;
+    erMsg = nameTooShortErrorMsg;
+  } else {
+    valid = true;
+  }
+
+  toggleErrorMsg(valid, nameError, erMsg);
   return valid;
 }
 
-function validateEmail() {
+function isEmailValid() {
   const email = emailInput.value;
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  valid = true;
+  let valid = true;
+  let erMsg = "";
 
   if (!email) {
-    pasteMsg(emailError, noEmailErrorMsg);
-    hideOrShowElem(emailError, false);
     valid = false;
+    erMsg = noEmailErrorMsg;
   } else if (!regex.test(email)) {
-    pasteMsg(emailError, invalidEmailErrorMsg);
-    hideOrShowElem(emailError, false);
     valid = false;
+    erMsg = invalidEmailErrorMsg;
   } else {
-    hideOrShowElem(emailError);
+    valid = true;
   }
+  toggleErrorMsg(valid, emailError, erMsg);
   return valid;
 }
 
@@ -103,22 +127,22 @@ function changeAgeOnInput() {
   ageInput.value = age.replace(/[^1-9]/g, "");
 }
 
-function validateAge() {
+function isAgeValid() {
   const age = ageInput.value;
-  let valid = true;
+  let valid;
+  let erMsg = "";
 
   if (!age) {
-    pasteMsg(ageError, noAgeErrorMsg);
-    hideOrShowElem(ageError, false);
     valid = false;
+    erMsg = noAgeErrorMsg;
   } else if (!ageInput.checkValidity()) {
-    pasteMsg(ageError, invalidAgeErrorMsg);
-    hideOrShowElem(ageError, false);
     valid = false;
+    erMsg = invalidAgeErrorMsg;
   } else {
-    hideOrShowElem(ageError);
+    valid = true;
   }
 
+  toggleErrorMsg(valid, ageError, erMsg);
   return valid;
 }
 
@@ -127,35 +151,123 @@ function markChecked(evt) {
     sexChecked = true;
   } else if (evt.target.type === "checkbox") {
     consentChecked = consentCheckbox.checked;
-    toggleBtnsErrorMsg(consentChecked, consentError, noConsentErrorMsg);
+    toggleErrorMsg(consentChecked, consentError, noConsentErrorMsg);
   }
 }
 
-function toggleBtnsErrorMsg(condition, element, msg) {
-  if (condition) {
-    pasteMsg(element, "");
-    hideOrShowElem(element);
-  } else {
-    pasteMsg(element, msg);
-    hideOrShowElem(element, false);
+function isOccupationValid() {
+  let valid = true;
+  let erMsg = "";
+
+  if (occupationSelect.selectedIndex === 0) {
+    valid = false;
+    erMsg = noOccupationErrorMsg;
   }
+
+  toggleErrorMsg(valid, occupationError, erMsg);
+  return valid;
+}
+
+function markUnmarkTipAsDone(elem, condition) {
+  if (condition) {
+    elem.classList.add("done");
+  } else {
+    elem.classList.remove("done");
+  }
+}
+
+function checkPassOnInput() {
+  toggleErrorMsg(true, passError, "");
+  hideOrShowElem(passTips, false);
+  const pass = passInput.value;
+  const upperCaseLetterRegex = /[A-Z]/;
+  const lowerCaseLetterRegex = /[a-z]/;
+  const digitRegex = /[1-9]/;
+
+  markUnmarkTipAsDone(lengthTip, pass.length >= 8);
+  markUnmarkTipAsDone(upperCaseTip, upperCaseLetterRegex.test(pass));
+  markUnmarkTipAsDone(lowerCaseTip, lowerCaseLetterRegex.test(pass));
+  markUnmarkTipAsDone(digitTip, digitRegex.test(pass));
+}
+
+function resetTips() {
+  hideOrShowElem(passTips);
+  const allTips = document.querySelectorAll(".reg-form__tips > li");
+  allTips.forEach(function (item) {
+    markUnmarkTipAsDone(item, false);
+  });
+}
+
+function isPassValid() {
+  const pass = passInput.value;
+  let valid;
+  let erMsg = "";
+  if (!pass) {
+    valid = false;
+    erMsg = noPassError;
+  } else if (!passInput.checkValidity()) {
+    valid = false;
+    erMsg = invalidPassError;
+  } else {
+    valid = true;
+  }
+
+  toggleErrorMsg(valid, passError, erMsg);
+  return valid;
+}
+
+function processPassOnBlur() {
+  resetTips();
+  isPassValid();
+}
+
+function isPassRepeatValid() {
+  const pass = passInput.value;
+  const passRepeat = passRepeatInput.value;
+  let valid;
+  let erMsg = "";
+
+  if (!passRepeat) {
+    valid = false;
+    erMsg = noRepeatPassError;
+  } else if (passRepeat !== pass) {
+    valid = false;
+    erMsg = invalidRepeatPassError;
+  } else {
+    valid = true;
+  }
+
+  toggleErrorMsg(valid, passRepeatError, erMsg);
+  return valid;
 }
 
 function submitForm(evt) {
   evt.preventDefault();
-  alert("Данные формы успешно отправлены");
+  alert("Данные успешно отправлены");
   regForm.reset();
 }
 
 btn.addEventListener("click", submitForm);
 nameInput.addEventListener("input", changeNameOnInput);
-nameInput.addEventListener("blur", validateName);
-emailInput.addEventListener("input", () => hideOrShowElem(emailError));
-emailInput.addEventListener("blur", validateEmail);
+nameInput.addEventListener("blur", isNameValid);
+
+emailInput.addEventListener("input", () =>
+  toggleErrorMsg(true, emailError, "")
+);
+emailInput.addEventListener("blur", isEmailValid);
+
 ageInput.addEventListener("input", changeAgeOnInput);
-ageInput.addEventListener("blur", validateAge);
+ageInput.addEventListener("blur", isAgeValid);
 radioGroups.forEach(function (item) {
   item.addEventListener("input", markChecked);
 });
 consentCheckbox.addEventListener("input", markChecked);
-// occupationSelect.addEventListener("blur", listener)
+occupationSelect.addEventListener("blur", isOccupationValid);
+
+passInput.addEventListener("input", checkPassOnInput);
+passInput.addEventListener("blur", processPassOnBlur);
+
+passRepeatInput.addEventListener("input", () =>
+  toggleErrorMsg(true, passRepeatError, "")
+);
+passRepeatInput.addEventListener("blur", isPassRepeatValid);
